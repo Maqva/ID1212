f72dd54270b585e7367f3c5d2745fcbe80688600
Magnusqv <Magnus.w.qvarnstrom@gmail.com>
2020-11-04 18:27:35 +0100

    Wrote task 1
    Finished the classes for lab assignment 1

--- ID1212Task1/src/chat/client/ChatClient.java
+++ ID1212Task1/src/chat/client/ChatClient.java
@@ -1,5 +1,12 @@
 package chat.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Has two threads, 
  * one to listen for incoming messages from the server 
@@ -8,17 +15,94 @@ package chat.client;
  */
 public class ChatClient {
 	private final int DEFAULT_CONNECTING_PORT = 5000;
+	private PrintWriter serverWriter;
+	private Scanner inputScanner = new Scanner(System.in);
+	private String hostAddress;
 	private int port;
+	private Thread listenerThread;
 	
-	public ChatClient(int serverPort) {
+	public ChatClient(String address, int serverPort) {
+		hostAddress = address;
 		port = serverPort;
 	}
-	public ChatClient() {
+	public ChatClient(String address) {
+		hostAddress = address;
 		port = DEFAULT_CONNECTING_PORT;
 	}
 	public static void main (String[] args) {
-		/**
-		 * Asks for the address and port to connect the client to.
-		 */
+		ChatClient client = null;
+		if(args.length >= 2) {
+			try {
+				int arg = Integer.parseInt(args[1]);
+				client = new ChatClient(args[0], arg);
+			}
+			catch(NumberFormatException e) {
+				System.out.println("\""+ args[1]+"\" is not a number, using default port...");
+				client = new ChatClient(args[0]);	
+			}
+		}
+		else if(args.length == 1){
+			System.out.println("using default port...");
+			client = new ChatClient(args[0]);
+		}
+		else {
+			System.out.println("Usage:: ChatClient HOST_ADDRESS [PORT_NUMBER]");
+		}
+		if(client != null) {
+			try {
+				client.connectToServer();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void connectToServer() throws IOException {
+		System.out.println("Connecting to "+hostAddress+" on port "+port);
+		Socket toConnect = new Socket(hostAddress, port);
+		serverWriter = new PrintWriter(toConnect.getOutputStream(), true);
+		listenerThread = new Thread(new ServerListener(toConnect));
+		listenerThread.start();
+		System.out.println("Connection succesfull!");
+		String message;
+		while (true) {
+			message = inputScanner.nextLine();
+			if(message.equalsIgnoreCase("exit")) {
+				System.out.println("Exiting...");
+				break;
+			}
+			else {
+				serverWriter.println(message);
+				serverWriter.flush();
+			}
+		}
+		listenerThread.interrupt();
+		toConnect.close();
+	}
+	
+	private class ServerListener implements Runnable{
+		private BufferedReader serverReader;
+		private final String FRM_SRVR_MSSG = "From Server: ";
+		
+		public ServerListener (Socket socket) throws IOException {
+			new BufferedReader(
+			        new InputStreamReader(socket.getInputStream()));	
+		}
+		@Override
+		public void run() {
+			String message;
+			while (true) {
+				try {
+					if((message = serverReader.readLine())!=null) {
+						System.out.println(FRM_SRVR_MSSG+message);
+					}
+				} catch (IOException e) {
+					System.err.println("A I/O Error occured while communicating with the server, disconnecting...");
+					break;
+				}
+			}
+		}
+		
 	}
 }
--- ID1212Task1/src/chat/server/ChatServer.java
+++ ID1212Task1/src/chat/server/ChatServer.java
@@ -1,6 +1,13 @@
 package chat.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Has one thread to each of the clients currently connected 
@@ -8,7 +15,8 @@ import java.net.Socket;
  * @author Magnus Qvarnström
  */
 public class ChatServer {
-	private final int DEFAULT_LISTENING_PORT = 5000;
+	private ArrayList<ClientHandler> clientList = new ArrayList<ClientHandler>();
+	private static final int DEFAULT_LISTENING_PORT = 5000;
 	private int serverPort;
 	
 	public ChatServer(int listeningPort) {
@@ -23,11 +31,39 @@ public class ChatServer {
 	 * @param args the port number to start the listening server on, uses DEFAULT_LISTENING_PORT if none provided.
 	 */
 	public static void main(String[] args) {
-		
+		ChatServer server;
+		if(args.length >= 1) {
+			try {
+				int arg = Integer.parseInt(args[0]);
+				server = new ChatServer(arg);
+			}
+			catch(NumberFormatException e) {
+				System.out.println("\""+ args[0]+"\" is not a number, using default port...");
+				server = new ChatServer();	
+			}
+		}
+		else {
+			System.out.println("using default port...");
+			server = new ChatServer();
+		}
+		try {
+			System.out.println("Starting Server...");
+			server.runServer();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Something went wrong when initiating server.");
+			e.printStackTrace();
+		}
 	}
 	
-	public void runServer() {
-		
+	public void runServer() throws IOException {
+		ServerSocket server = new ServerSocket(serverPort);
+		Socket clientSocket;
+		System.out.println("Started server on port: "+serverPort);
+		while((clientSocket = server.accept()) != null) {
+			handleConnection(clientSocket);
+		}
+		server.close();
 	}
 	
 	/**
@@ -35,8 +71,69 @@ public class ChatServer {
 	 * @param client
 	 */
 	private void handleConnection(Socket client) {
+		try {
+			ClientHandler handler = new ClientHandler(this, client);
+			synchronized (clientList) {
+				clientList.add(handler);
+			}
+			Thread t = new Thread(handler);
+			t.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
-	prvate
+	public void broadcastMessage(String message, ClientHandler sender) {
+		synchronized (clientList){
+			for (ClientHandler cH : clientList) {
+				if(cH != sender)
+					cH.addMsgToQueue(message);
+			}
+		}
+	}
+	
+	private class ClientHandler implements Runnable{
+		public boolean connected = true;
+		private ChatServer server;
+		private ConcurrentLinkedQueue<String> messageQueue = new ConcurrentLinkedQueue<String>();
+		private BufferedReader clientInputReader;
+		private PrintWriter clientOutputWriter;
+		
+		public ClientHandler (ChatServer server, Socket connectingClient) throws IOException {
+			this.server = server;
+			clientOutputWriter = new PrintWriter(connectingClient.getOutputStream(), true);
+			clientInputReader = new BufferedReader(
+			        new InputStreamReader(connectingClient.getInputStream()));
+		}
+
+		public void addMsgToQueue(String message) {
+			messageQueue.add(message);
+		}
+		
+		@Override
+		public void run() {
+			String messageToSend, readInput;
+			while(connected) {
+				if(messageQueue.isEmpty()) {
+						try {
+							if((readInput = clientInputReader.readLine()) != null) {
+								server.broadcastMessage(readInput, this);
+							}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+				}
+				else{
+					messageToSend = messageQueue.poll();
+					clientOutputWriter.println(messageToSend);
+					clientOutputWriter.flush();
+				}
+			}
+		}
+		
+	}
+	
 }
